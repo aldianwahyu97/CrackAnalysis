0a119bd2838d6874d3ae2f667245a5410acc770f
unknown <aldianwahyuseptiadi1997@gmail.com>
2019-06-22 12:12:42 +0800

    Tambah Splashscreen dan perbaiki tampilan

---
+++ app/src/main/java/com/example/crackanalysis/SplashCrack.java
@@ -0,0 +1,14 @@
+package com.example.crackanalysis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashCrack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_crack);
+    }
+}
